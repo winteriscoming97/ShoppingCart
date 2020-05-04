c89c0335109406fa64548565c3711b696c37903f
Justin <justinjackson1198@gmail.com>
2020-05-04 15:59:26 -0700

    individual item totals and input fields provided

--- js/application.js
+++ js/application.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
-var calculateTotalPrice = function () {
-
-}
-
-
+  calculateTotalPrice();
 });
+
+
+var calculateTotalPrice = function () {
+  $('tbody tr').each(function (index, element) {
+    if (index > 0) {
+      var price = parseFloat($(this).children('.price').text().split('').slice(1).join(''));
+
+      var quantity = $(this).children('.quantity').children('input').val();
+      var total = price * quantity;
+
+      $(this).children('.item-total').text('$' + String(total.toFixed(2)));
+
+    }
+  });
+}
